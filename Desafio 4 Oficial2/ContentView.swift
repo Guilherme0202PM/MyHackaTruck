d2f1bac94308c44b2327a1d87250e7f4953a45f1
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2023-08-02 14:28:03 -0300

    Desafio 4.3

--- Desafio 4 Oficial2/ContentView.swift
+++ Desafio 4 Oficial2/ContentView.swift
@@ -12,19 +12,19 @@ struct ContentView: View {
     let song = [Song(id: 1, name: "Viva la Vida", artist: "Coldplay",capa:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtjnJ0wKGRvg2VV3n27H8wwJ2xZPTNQwevVA&usqp=CAU"),
                 Song(id: 2, name: "Yellow", artist: "Coldplay",
                      capa:"https://i1.sndcdn.com/artworks-000402447507-zyjqsu-t500x500.jpg"),
-                     Song(id: 3, name: "Chandelier", artist: "Sia", capa:"https://i.ytimg.com/vi/2vjPBrBU-TM/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCrBoC7iIkACGu4zzJmcRrVdVS04Q"),
-                     Song(id: 4, name: "A Sky Full of Sky", artist: "Coldplay", capa:"https://vivacoldplay.com/wp-content/uploads/2015/11/CTHS4RZWUAAsMIV.jpg"),
+                Song(id: 3, name: "Chandelier", artist: "Sia", capa:"https://i.ytimg.com/vi/2vjPBrBU-TM/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCrBoC7iIkACGu4zzJmcRrVdVS04Q"),
+                Song(id: 4, name: "A Sky Full of Sky", artist: "Coldplay", capa:"https://vivacoldplay.com/wp-content/uploads/2015/11/CTHS4RZWUAAsMIV.jpg"),
                 Song(id: 5, name: "Viva la Vida", artist: "Coldplay",capa:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtjnJ0wKGRvg2VV3n27H8wwJ2xZPTNQwevVA&usqp=CAU"),
-                            Song(id: 6, name: "Yellow", artist: "Coldplay",
-                                 capa:"https://i1.sndcdn.com/artworks-000402447507-zyjqsu-t500x500.jpg"),
-                                 Song(id: 7, name: "Chandelier", artist: "Sia", capa:"https://i.ytimg.com/vi/2vjPBrBU-TM/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCrBoC7iIkACGu4zzJmcRrVdVS04Q"),
-                                 Song(id: 8, name: "A Sky Full of Sky", artist: "Coldplay", capa:"https://vivacoldplay.com/wp-content/uploads/2015/11/CTHS4RZWUAAsMIV.jpg"),
-                     
-                     ]
-                     
-let sugestao = [Sugestao(id: 1, albumCover: "Viva la Vida", albumName:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtjnJ0wKGRvg2VV3n27H8wwJ2xZPTNQwevVA&usqp=CAU")]
-                     
-                     var body: some View {
+                Song(id: 6, name: "Yellow", artist: "Coldplay",
+                     capa:"https://i1.sndcdn.com/artworks-000402447507-zyjqsu-t500x500.jpg"),
+                Song(id: 7, name: "Chandelier", artist: "Sia", capa:"https://i.ytimg.com/vi/2vjPBrBU-TM/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCrBoC7iIkACGu4zzJmcRrVdVS04Q"),
+                Song(id: 8, name: "A Sky Full of Sky", artist: "Coldplay", capa:"https://vivacoldplay.com/wp-content/uploads/2015/11/CTHS4RZWUAAsMIV.jpg"),
+                
+    ]
+    
+    let sugestao = [Sugestao(id: 1, albumCover: "Viva la Vida", albumName:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtjnJ0wKGRvg2VV3n27H8wwJ2xZPTNQwevVA&usqp=CAU"),Sugestao(id: 2, albumCover: "A Sky Full of Sky", albumName:"https://vivacoldplay.com/wp-content/uploads/2015/11/CTHS4RZWUAAsMIV.jpg"),Sugestao(id: 3, albumCover: "Chandelier", albumName:"https://i.ytimg.com/vi/2vjPBrBU-TM/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCrBoC7iIkACGu4zzJmcRrVdVS04Q"),Sugestao(id: 4, albumCover: "Yellow", albumName:"https://i1.sndcdn.com/artworks-000402447507-zyjqsu-t500x500.jpg"),]
+    
+    var body: some View {
         NavigationStack{
             ZStack{ // ativa o fundo
                 
@@ -53,22 +53,22 @@ let sugestao = [Sugestao(id: 1, albumCover: "Viva la Vida", albumName:"https://e
                             ForEach(song) { song in //ele percorre meu vetor
                                 
                                 HStack{
-                                AsyncImage(url:
-                                            URL(string: song.capa)) {
-                                    song in
-                                    song.resizable().frame(width: 60, height: 60)
-                                    
-                                    
-                                } placeholder: { //caso a image falhe fica "carregando"
-                                    ProgressView()
-                                }
+                                    AsyncImage(url:
+                                                URL(string: song.capa)) {
+                                        song in
+                                        song.resizable().frame(width: 60, height: 60)
+                                        
+                                        
+                                    } placeholder: { //caso a image falhe fica "carregando"
+                                        ProgressView()
+                                    }
                                     NavigationLink(destination: Text(song.name)){ //nome vira link
                                         VStack{
-                                        Text(song.name).foregroundColor(.white)
-                                            .font(.system(size: 20))
-                                      
-                                        
-                                   
+                                            Text(song.name).foregroundColor(.white)
+                                                .font(.system(size: 20))
+                                            
+                                            
+                                            
                                             Text(song.artist).foregroundColor(.white).font(.system(size: 15))
                                             
                                         }
@@ -76,9 +76,45 @@ let sugestao = [Sugestao(id: 1, albumCover: "Viva la Vida", albumName:"https://e
                                     }
                                 }
                             }
+                            
+                            HStack{
+                                Text("Sugeridos").foregroundColor(.white).font(.system(size: 50))
+                                Spacer()
+                            }
                         }
                         
-                    }
+                        
+                        ScrollView(.horizontal, showsIndicators: false){ // false o scrol na horizontal
+                            HStack{
+                                
+                                
+                                ForEach(sugestao) { sugestao in //ele percorre meu vetor
+                                    
+                                    HStack{
+                                        AsyncImage(url:
+                                                    URL(string: sugestao.albumName)) {
+                                            sugestao in
+                                            sugestao.resizable().frame(width: 200, height: 200)
+                                            
+                                            
+                                        } placeholder: { //caso a image falhe fica "carregando"
+                                            ProgressView()
+                                        }
+                                        /*NavigationLink(destination: Text(sugestao.albumName)){ //nome vira link
+                                            HStack{
+                                                Text(sugestao.albumCover).foregroundColor(.white)
+                                                    .font(.system(size: 20))
+                                                
+                                                
+                                            //}
+                                            
+                                        }*/
+                                    }
+                                }
+                                
+                            }// Fim do Scrol sugestao
+                        }// Sugestao
+                    } // Fim do Scron de tudo
                     
                     
                 }
@@ -87,12 +123,12 @@ let sugestao = [Sugestao(id: 1, albumCover: "Viva la Vida", albumName:"https://e
         }
     }
 }
-                     
-                     struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
 
 
 
---
+++ Desafio 4 Oficial2/Desafio_4_OficiaalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_4_OficiaalApp.swift
+//  Desafio 4 Oficiaal
+//
+//  Created by Student10 on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_4_OficiaalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
