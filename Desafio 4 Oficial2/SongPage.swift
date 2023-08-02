4fa2e30728f4b0a2796eb282e01929c51ce54ead
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2023-08-02 15:49:55 -0300

    Desafio 4.6 Ajuste

--- Desafio 4 Oficial2/SongPage.swift
+++ Desafio 4 Oficial2/SongPage.swift
@@ -18,32 +18,31 @@ struct SongPage: View{
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
             
             VStack{
+                
                 AsyncImage(url: URL(string: song.capa)) {
                     song in
-                    song.resizable().frame(width: 60, height: 60)
+                    song.resizable().frame(width: 200, height: 200)
                 } placeholder: { //caso a image falhe fica "carregando"
                     ProgressView()
                 }
                 
                     VStack{
                         Text(song.name).foregroundColor(.white)
-                            .font(.system(size: 20))
+                            .font(.system(size: 40))
                         
-                        Text(song.artist).foregroundColor(.white).font(.system(size: 15))
-                        
+                        Text(song.artist).foregroundColor(.white).font(.system(size: 25))
                     }
-                
                 HStack{
                     Image(systemName: "shuffle").resizable().frame(width: 30, height: 30)
-                        .imageScale(.large).padding()
+                        .imageScale(.large).padding().offset(x:0 , y: 150)
                         Image(systemName: "backward.end.fill").resizable().frame(width: 30, height: 30)
-                        .imageScale(.large).padding()
+                        .imageScale(.large).padding().offset(x:0 , y: 150)
                         Image(systemName: "play.fill").resizable().frame(width: 40, height: 40)
-                        .imageScale(.large).padding()
+                        .imageScale(.large).padding().offset(x:0 , y: 150)
                         Image(systemName: "forward.end.fill").resizable().frame(width: 30, height: 30)
-                        .imageScale(.large).padding()
+                        .imageScale(.large).padding().offset(x:0 , y: 150)
                         Image(systemName: "repeat").resizable().frame(width: 30, height: 30)
-                        .imageScale(.large).padding()
+                        .imageScale(.large).padding().offset(x:0 , y: 150)
                 }.foregroundColor(.white)
             }
         }
