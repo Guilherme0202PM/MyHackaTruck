eabc648919238f2ca339fc423644d351e457bd74
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2023-08-07 14:48:00 -0300

    (File) Desafio 6 (API) .2

--- Desafio 6/ContentView.swift
+++ Desafio 6/ContentView.swift
@@ -10,13 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        ZStack{
+            AsyncImage(url: URL(string: "https://img.freepik.com/vetores-premium/pixel-art-retro-wave-scifi-background-com-nascer-ou-por-do-sol-pixel-art-80s-pixel-art-jogo-de-8-bits_148553-568.jpg?w=2000"))
+
+                
+        placeholder: do {
+            ProgressView()
+            }
+            
+            /*AsyncImage(url: url) { image in  // <-- here
+                     image.resizable()
+                 } placeholder: {
+                     ProgressView().progressViewStyle(.circular)
+                 }*/
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                //Image(systemName: "globe").imageScale(.large).foregroundColor(.accentColor)
+                //Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
