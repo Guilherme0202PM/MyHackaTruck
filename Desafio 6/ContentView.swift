1df194d22869a923d9171a769a7f14c476954fe1
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2023-08-08 14:04:55 -0300

    Desafio 6 (API SheetView funcionou) .9

--- Desafio 6/ContentView.swift
+++ Desafio 6/ContentView.swift
@@ -9,7 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @StateObject var viewModel = ViewModel()
+    @State public var showingSheet = false
+
     var body: some View {
        
         //@State var lista = [games]
@@ -30,12 +33,17 @@ struct ContentView: View {
                 ScrollView(){
                     ForEach(viewModel.jogos, id: \.id) { jogo in
                         Button(action: {
-                             // Implementar ação para levar para a TabView com as informações detalhadas
                             
+                            showingSheet.toggle()
+                            //.sheet(isPresented: $showingSheet)
                         }) {
                             VStack(spacing: 8) {
                                 AsyncImage(url: URL(string: jogo.thumbnail ?? "")) { image in
                                     image.resizable().aspectRatio(contentMode: .fill).frame(width: 250, height: 250).cornerRadius(8)
+                                        
+                                        .sheet(isPresented: $showingSheet){
+                                        GameDetailView(jogo: games(id: 1136, title: "Overwatch 2", thumbnail:"https://www.mmobomb.com/g/1136/thumbnail.jpg", short_description: "Big changes come to the Overwatch formula in this sequel...and so does PvE content, eventually.", game_url: "https://www.mmobomb.com/open/overwatch-2", genre: "Shooter", platform: "PC (Windows)", publisher: "Activision Blizzard King", developer: "Blizzard Entertainment", release_date: "2022-10-04", profile_url: "https://www.mmobomb.com/overwatch-2"))
+                                    }//fim da sheet
                                 }
                             placeholder: {
                                 ProgressView()
@@ -47,6 +55,11 @@ struct ContentView: View {
                                     .multilineTextAlignment(.center)
                                     .foregroundColor(.gray)
                                     .font(.system(size: 35))
+                                
+                               // sheet(isPresented: $showingSheet) {
+                                // Conteúdo da folha que você deseja exibir
+                                //Text("Sheet content")
+
                             }
                             
                         }
